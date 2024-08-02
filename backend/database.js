90dbb9c47cbc725bcc327c39afc68ba49494b93d
unknown <Rashad.mohd2004@gmail.com>
2024-08-02 14:59:04 +0530

    finish

--- backend/database.js
+++ backend/database.js
@@ -121,7 +121,7 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, religion) => {
-    const sql = `INSERT INTO student(id,name,age,religion) values (?, ?, ?, ?)`
+    const sql = `INSERT INTO student(id,name,age,religion) values(?, ?, ?, ?)`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql,[id,name,age,religion])
